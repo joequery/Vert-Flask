7716f8c59312d6dbe342822eadfe7c3bc2e99fdf
Joseph McCullough <joseph@vertstudios.com>
2012-06-01 14:21:20 -0500

    Add about and services pages

--- vertstudios.py
+++ vertstudios.py
@@ -30,5 +30,13 @@ def home():
 def work():
   return render_template("work.html")
 
+@app.route('/services')
+def services():
+  return render_template("services.html")
+
+@app.route('/about')
+def about():
+  return render_template("about.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)

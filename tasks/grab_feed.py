28f6a9add2ca6a4566857d5046850602c46dcad4
Joseph McCullough <joseph@vertstudios.com>
2012-06-03 02:16:50 -0500

    Finally created propert import

--- tasks/grab_feed.py
+++ tasks/grab_feed.py
@@ -1,8 +1,10 @@
 import os
 
-# We're 2 away from the parent directory (PARENT/tasks/somefile.py)
+# Hack to import modules from a sibling directory. Ew >_>
+# parentDir is just the root directory of the project.
 parentDir = os.path.abspath(__file__).rsplit('/', 2)[0]
-import sys; sys.path.insert(0, parentDir)
-import helpers
+vertDir = os.path.join(parentDir, "vertstudios")
+import sys; sys.path.append(vertDir)
+import helpers.rss
 
-print(dir(helpers))
+print(dir(helpers.rss))

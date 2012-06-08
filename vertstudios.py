cdeeb9b88459c3604dae570796a8030110028076
Joseph McCullough <joseph@vertstudios.com>
2012-06-08 17:43:36 -0500

    Use url_for in navigation

--- vertstudios.py
+++ vertstudios.py
@@ -41,5 +41,9 @@ def services():
 def about():
   return render_template("about.html")
 
+@app.route('/contact')
+def contact():
+  return render_template("contact.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)

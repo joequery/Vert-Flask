af0cd7933dfae85fcc4c3261bb1a70e201e832d9
Joseph McCullough <joseph@vertstudios.com>
2012-06-11 09:06:16 -0500

    Get FLASK_ENV in settings file

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Global setting variables and etc go here.
+import os
 
 # Where cache files are stored
 CACHE_DIR = "/tmp"
@@ -9,3 +10,9 @@ CACHE_DIR = "/tmp"
 NUM_BLOG_POSTS = 5
 BLOG_CACHE_FILE = "vert_rss.json"
 FEED_CACHE_TIME = 3600 # 1 hour blog feed cache
+
+# FLASK_ENV environment variable can be 'development' or 'production'
+if "FLASK_ENV" in os.environ.keys():
+	FLASK_ENV = os.environ["FLASK_ENV"]
+else:
+	FLASK_ENV = "development"

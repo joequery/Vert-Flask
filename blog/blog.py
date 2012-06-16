c6627435d118b685bbbdf8edb9c44923f7bd204d
Joseph McCullough <joseph@vertstudios.com>
2012-06-16 11:11:11 -0500

    Get templates and posts directories working

--- blog/blog.py
+++ blog/blog.py
@@ -6,7 +6,7 @@ from flask import (
 )
 from jinja2 import TemplateNotFound
 
-blog = Blueprint('blog', __name__, template_folder="templates")
+blog = Blueprint('blog', __name__, template_folder="./")
 
 @blog.errorhandler(404)
 def page_not_found(e):
@@ -15,12 +15,12 @@ def page_not_found(e):
 @blog.route('/blog/')
 @blog.route('/blog')
 def blog_index():
-  return render_template("blog_index.html")
+  return render_template("templates/blog_index.html")
 
 @blog.route('/blog/<post>')
 def blog_post(post):
   try:
-    return render_template("blog_posts/%s.html" % post)
+    return render_template("posts/%s.html" % post)
   except TemplateNotFound:
     return page_not_found(TemplateNotFound)
 

d3fbcbb7df2fccf2e3d597125c913deeeb070edb
Joseph McCullough <joseph@vertstudios.com>
2012-07-03 17:11:23 -0500

    Begin import attempts

--- blog/bulk_import.py
+++ blog/bulk_import.py
@@ -23,23 +23,73 @@ for post in posts:
   content = jQuery(post).children("content").html()
   excerpt = jQuery(post).children("excerpt").html()
 
+  ##################################################################
+  # Prepare variables
+  ##################################################################
+
   # Isolate the url, since this will become our directory.
   # http://www.vertstudios.com/blog/some-post => some-post
   # We ultimately want posts/some-post/
   directory = os.path.join("posts", link.split('/')[-2])
 
-  # Parse the date provided into a time object. We start off with
-  # Sat, 06 Feb 2010 07:41:48 +0000. We get rid of Sat, and the +0000
+  # Parse the date provided into a time object. 
+  # "Sat, 06 Feb 2010 07:41:48 +0000" =>  "06 Feb 2010 07:41:48"
   pubdate = pubdate.split(',')[1].strip()[:-6]
-
-  # Now we have "06 Feb 2010 07:41:48", which is parseable.
   pubdateObj = strptime(pubdate, "%d %b %Y %H:%M:%S")
 
   # With the time object, we can now put the date in the form of the timestamp
-  # We'll use when writing articles: 2012-07-03 Tue 04:37 PM
+  # we'll use when writing articles: 2012-07-03 Tue 04:37 PM
   pubdate = strftime("%Y-%m-%d %a %H:%M %p", pubdateObj)
 
   # Get the post content into markdown.
   markdown = html2text(content)
 
+  # posts/some-post/body.html
+  bodyfilePath = os.path.join(directory, "body.html")
+  metafilePath = os.path.join(directory, "meta.py")
+
+  ##################################################################
+  # Begin execution!
+  ##################################################################
+
   os.mkdir(directory)
+
+  bodyfile = open(bodyfilePath, 'w')
+  metafile = open(bodyfilePath, 'w')
+
+  # Write the body
+  bodyfile.write(
+    """
+    {% extends "templates/post.html" %}
+    {% block post %}
+    {% filter markdown %}
+    [{{post['title']}}]("{{request.url}}")
+    ======================================
+
+    """
+  )
+  bodyfile.write(markdown)
+  bodyfile.write(
+    """
+    {% endfilter %}
+    {% endblock post %}
+    """
+  )
+
+  # Write the metadata
+
+  metafile.write(
+    """
+      title="%s"
+      description="%s"
+      time="%s"
+      excerpt="%s"
+    """ % (title, description, pubdate, excerpt)
+  )
+
+  ##################################################################
+  # Clean up
+  ##################################################################
+
+  bodyfile.close()
+  metafile.close()

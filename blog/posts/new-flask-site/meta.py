30d6122eb8a3a1bdd715374b2dc61514bef5152e
Joseph McCullough <joseph@vertstudios.com>
2012-07-06 20:15:38 -0500

    Begin working on new flask site blog post

---
+++ blog/posts/new-flask-site/meta.py
@@ -0,0 +1,7 @@
+
+title="Powered by Flask: Converting a nearly static website to Python Flask"
+description=""
+time="2012-07-05 Thu 02:53 PM"
+excerpt="""Post excerpt goes here. You can make it as long as
+you want. Make sure to use triple string!"""
+

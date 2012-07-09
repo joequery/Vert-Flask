4b1833f580d132d205aa32d1140c0942ad7b0d7c
Joseph McCullough <joseph@vertstudios.com>
2012-07-09 14:38:54 -0500

    Added support for descriptions

--- blog/posts/new-flask-site/meta.py
+++ blog/posts/new-flask-site/meta.py
@@ -1,6 +1,7 @@
 
 title="Converting a simple website to Python Flask"
-description=""
+description="""Documenting first impressions converting a simple PHP/Wordpress
+site to Python Flask."""
 time="2012-07-09 Mon 10:17 AM"
 excerpt="""Flask is a Python web framework that's steadily increasing in 
 popularity within the webdev world. After reading so many great things about 

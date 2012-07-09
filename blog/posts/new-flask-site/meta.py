1b1f766539fe2c6cb3b8f7aa155ad7094a9598f3
Joseph McCullough <joseph@vertstudios.com>
2012-07-09 10:30:55 -0500

    Finish public new flask post

--- blog/posts/new-flask-site/meta.py
+++ blog/posts/new-flask-site/meta.py
@@ -1,7 +1,8 @@
 
 title="Converting a simple website to Python Flask"
 description=""
-time="2012-07-05 Thu 02:53 PM"
-excerpt="""Post excerpt goes here. You can make it as long as
-you want. Make sure to use triple string!"""
+time="2012-07-09 Mon 10:17 AM"
+excerpt="""Flask is a Python web framework that's steadily increasing in 
+popularity within the webdev world. After reading so many great things about 
+Flask, I decided to try it out myself."""
 

0d280c6ca75624b3a61f5be386d98eca81ccd0a7
Joseph McCullough <joseph@vertstudios.com>
2012-07-10 23:41:14 -0500

    Add post to rss feed and fix excerpt

--- blog/posts/structures-in-c/meta.py
+++ blog/posts/structures-in-c/meta.py
@@ -3,6 +3,5 @@ title="Structures in C Tutorial: The Basics"
 description="""A brief introduction to using Structures in C. Covers 
 assignments, passing to function parameters, and structure declaration."""
 time="2012-07-10 Tue 11:36 PM"
-excerpt="""Post excerpt goes here. You can make it as long as
-you want. Make sure to use triple string!"""
-
+excerpt="""This post serves as an introduction to using Structures in C! This 
+is the first of many C tutorials I plan on writing."""

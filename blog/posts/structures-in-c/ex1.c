5250e7df63feda0f86e40cd39f85ebefe69eab06
Joseph McCullough <joseph@vertstudios.com>
2012-07-10 21:56:04 -0500

    Begin working on structures in C post

---
+++ blog/posts/structures-in-c/ex1.c
@@ -0,0 +1,14 @@
+// Using structures in C
+#include<stdio.h>
+
+struct person{
+  char name[31];
+  int age;
+  char gender;
+};
+
+int main(){
+  printf("Hello, world!\n");
+  return 0;
+}
+
---
+++ blog/posts/structures-in-c/meta.py
@@ -0,0 +1,7 @@
+
+title="Structures in C"
+description="An introduction to using Structures in C"
+time="2012-07-10 Tue 09:45 PM"
+excerpt="""Post excerpt goes here. You can make it as long as
+you want. Make sure to use triple string!"""
+

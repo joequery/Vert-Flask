6daf283cd559b9812366544a69dd3ea8b6ab7eb1
Joseph McCullough <joseph@vertstudios.com>
2012-07-12 00:56:03 -0500

    Begin how to read a programming tutorial post

---
+++ blog/posts/how-to-read-programming-tutorial/meta.py
@@ -0,0 +1,11 @@
+
+title="How to Read a Programming Tutorial"
+description="""Recommended steps to get the most out of a programming tutorial."""
+time="2012-07-11 Wed 10:59 PM"
+excerpt="""Many of us seek to improve ourselves beyond what's required. Many 
+CS students are not content to just make good grades in class; they want to 
+get their hands dirty with real-world projects. Many employees read up on the 
+latest languages and frameworks well beyond office hours, simply because they 
+want to play with technology while learning to build a better product. It's 
+important, however, to ensure our time spent studying new programming 
+techniques does not go to waste."""

ff48b7ee2b23273182c56ae0c098f239ca427773
Joseph McCullough <joseph@vertstudios.com>
2012-07-15 13:49:27 -0500

    Begin working on twitchtv iphone post. Move blog images

---
+++ blog/posts/twitchtv-landscape-lock/meta.py
@@ -0,0 +1,8 @@
+
+title="Lock TwitchTV in Landscape Mode on your iPhone"
+description="""Step by step guide on how to lock your iPhone in Landscope mode
+when watching TwitchTV"""
+time="2012-07-05 Thu 02:53 PM"
+excerpt="""Post excerpt goes here. You can make it as long as
+you want. Make sure to use triple string!"""
+

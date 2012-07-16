420580a2f8d291b066f3f11cf11fed27d439bbf0
Joseph McCullough <joseph@vertstudios.com>
2012-07-15 21:22:03 -0500

    Finish twitchtv iPhone app post

--- blog/posts/twitchtv-landscape-lock/meta.py
+++ blog/posts/twitchtv-landscape-lock/meta.py
@@ -1,8 +1,7 @@
 
 title="Lock TwitchTV in Landscape Mode on your iPhone"
 description="""Step by step guide on how to lock your iPhone in Landscope mode
-when watching TwitchTV"""
-time="2012-07-05 Thu 02:53 PM"
-excerpt="""Post excerpt goes here. You can make it as long as
-you want. Make sure to use triple string!"""
-
+when watching TwitchTV."""
+time="2012-07-15 Sun 03:27 PM"
+excerpt = """Step by step guide on how to lock your iPhone in Landscope mode
+when watching TwitchTV."""

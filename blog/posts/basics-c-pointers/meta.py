b478c5ebfd5a0b047e2fb8b0d3abfaca0b4fbd95
Joseph McCullough <joseph@vertstudios.com>
2012-07-18 23:09:27 -0500

    Rename C tutorial

--- blog/posts/basics-c-pointers/meta.py
+++
@@ -1,3 +0,0 @@
-title="[Screencast] Basics of C Pointers"
-description="An easy to understand introduction to using pointers in C."
-time="2012-07-16 Mon 07:29 PM"
---
+++ blog/posts/introduction-pointers-in-c/meta.py
@@ -0,0 +1,3 @@
+title="[Screencast] Beginner's Introduction to Pointers in C"
+description="An easy, accessible introduction to using pointers in C."
+time="2012-07-18 Wed 11:03 PM"

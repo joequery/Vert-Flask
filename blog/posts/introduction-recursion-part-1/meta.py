bfc76bf6cdd021cbdc57cef9bf99c4c765212283
Joseph McCullough <joseph@vertstudios.com>
2012-07-20 19:51:23 -0500

    Add related articles to a few select posts

--- blog/posts/introduction-recursion-part-1/meta.py
+++ blog/posts/introduction-recursion-part-1/meta.py
@@ -3,4 +3,6 @@ title="""Introduction to Recursion - Part 1"""
 description=""""""
 time="""2012-03-31 Sat 02:36 AM"""
 excerpt="""Part 1 of a multipart series covering the basics of recursion.""" 
-
+related=[
+    ("Introduction to Recursion - Part 2", "introduction-recursion-part-2")
+]
--- blog/posts/introduction-recursion-part-2/meta.py
+++ blog/posts/introduction-recursion-part-2/meta.py
@@ -3,4 +3,6 @@ title="""Introduction to Recursion - Part 2"""
 description=""""""
 time="""2012-04-16 Mon 06:01 AM"""
 excerpt="""Part two of the <a href="http://www.vertstudios.com/blog/introduction-recursion-part-1">Introduction to Recursion series</a>! In this video, we discuss the Factorial function and its recursive properties in depth.""" 
-
+related=[
+    ("Introduction to Recursion - Part 1", "introduction-recursion-part-1")
+]

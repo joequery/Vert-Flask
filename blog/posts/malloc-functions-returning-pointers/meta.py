e42840649c4c34819026e939172992b25e8a9d67
Joseph McCullough <joseph@vertstudios.com>
2012-07-22 16:13:01 -0500

    Begin working on malloc blog post

---
+++ blog/posts/malloc-functions-returning-pointers/meta.py
@@ -0,0 +1,4 @@
+title="C: malloc and functions returning pointers"
+description="""Explanation and examples of the malloc function. Also, how to
+create/use functions that return pointers."""
+time="2012-07-22 Sun 03:32 PM"

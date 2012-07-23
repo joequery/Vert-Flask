6085b0a5a48e867e35c9be9de83d83d7689e7b29
Joseph McCullough <joseph@vertstudios.com>
2012-07-23 11:59:32 -0500

    Add related posts, add section about free

--- blog/posts/malloc-functions-returning-pointers/meta.py
+++ blog/posts/malloc-functions-returning-pointers/meta.py
@@ -2,3 +2,9 @@ title="C: malloc and functions returning pointers"
 description="""Explanation and examples of the malloc function. Also, how to
 create/use functions that return pointers."""
 time="2012-07-22 Sun 03:32 PM"
+related=[
+  ("[Screencast] Beginner's Introduction to Pointers in C", 
+                              "introduction-pointers-in-c"),
+  ("Structures in C Tutorial: The Basics", "structures-in-c"),
+  ("How to Read a Programming Tutorial", "how-to-read-programming-tutorial")
+]

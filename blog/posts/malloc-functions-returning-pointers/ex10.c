e190a26d2a1192e30a7cd9fe84346cb95d7c7e4f
Joseph McCullough <joseph@vertstudios.com>
2012-07-25 02:39:57 -0500

    Almost finished with malloc article: need to not use malloc in void section

--- blog/posts/malloc-functions-returning-pointers/ex10.c
+++ blog/posts/malloc-functions-returning-pointers/ex10.c
@@ -7,9 +7,16 @@ void* new_integer(void);
 int main(){
   // Get the address of an integer
   int *p = new_integer();
+
+  // Even though new_integer() returns a void pointer, it's implicitly 
+  // converted to int* in the assignment above. p is a pointer to int,
+  // not a void pointer, so it can safely be dereferenced. 
   *p = 15;
 
-  printf("The value at the address of vp is: %d\n", *(int*)vp);
+  printf("The value at the address of p is: %d\n", *p);
+
+  float x = 10;
+  printf("x: %.2f\n", x);
   return 0;
 }
 

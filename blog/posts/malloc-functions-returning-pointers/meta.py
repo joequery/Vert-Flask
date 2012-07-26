cb724005cbe0a5032365aba5d4c18900ac0aad0a
Joseph McCullough <joseph@vertstudios.com>
2012-07-25 19:32:00 -0500

    Finished recording, youtube video link placed in

--- blog/posts/malloc-functions-returning-pointers/meta.py
+++ blog/posts/malloc-functions-returning-pointers/meta.py
@@ -1,6 +1,6 @@
-title="C: malloc and functions returning pointers"
-description="""Explanation and examples of the malloc function. Also, how to
-create/use functions that return pointers."""
+title="[Screencast] C: malloc and functions returning pointers"
+description="""Explanation and examples of using the malloc function. Also, how
+to create/use functions that return pointers."""
 time="2012-07-22 Sun 03:32 PM"
 related=[
   ("[Screencast] Beginner's Introduction to Pointers in C", 
